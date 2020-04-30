f07ead6fcba297171830cca91ba924b3734b66f5
louis1001 <luisal1207@gmail.com>
2020-04-30 15:12:28 -0600

    Gets rid of <filesystem> because it's not stable.
    Apparently, who knows. Regardless, the functionality I needed was super simple, so I don't need that module anyway.

--- src/IO/io.cpp
+++ src/IO/io.cpp
@@ -4,12 +4,10 @@
 
 #include "IO/io.h"
 #include <fstream>
-#include <filesystem>
 #include <Exceptions/exception.h>
 
 namespace Odo::io {
     std::string read_file(const std::string& path) {
-        std::filesystem::path a(path);
         std::ifstream argumentFile(path);
         if (argumentFile.fail()) {
             throw Exceptions::IOException(path);
@@ -24,13 +22,8 @@ namespace Odo::io {
     }
 
     std::string get_file_name(const std::string& path) {
-        std::filesystem::path p(path);
-        std::string result;
-        std::string ext = p.extension();
-        auto fn = p.filename().string();
-        for (auto i = 0; i < fn.length()-ext.length(); i++) {
-            result += fn[i];
-        }
+        std::size_t found = path.find_last_of("/\\");
+        std::string result = path.substr(found+1);
 
         return result;
     }
