a710d5c096890cca200093bf0bd0829073af5e59
louis1001 <luisal1207@gmail.com>
2020-07-08 13:42:37 -0600

    Adds a forward definition header for ast nodes.
    Just so I don't have to include all nodes one by one unless necessary.

---
+++ include/Parser/AST/Forward.h
@@ -0,0 +1,51 @@
+//
+// Created by Luis Gonzalez on 7/8/20.
+//
+
+#ifndef ODO_FORWARD_H
+#define ODO_FORWARD_H
+namespace Odo::Parsing {
+    struct DoubleNode;
+    struct IntNode;
+    struct BoolNode;
+    struct StrNode;
+    struct TernaryOpNode;
+    struct BinOpNode;
+    struct UnaryOpNode;
+    struct NoOpNode;
+    struct VarDeclarationNode;
+    struct ListDeclarationNode;
+    struct VariableNode;
+    struct AssignmentNode;
+    struct ListExpressionNode;
+    struct BlockNode;
+    struct FuncExpressionNode;
+    struct FuncDeclNode;
+    struct FuncCallNode;
+    struct FuncBodyNode;
+    struct ReturnNode;
+    struct IfNode;
+    struct ForNode;
+    struct ForEachNode;
+    struct FoRangeNode;
+    struct WhileNode;
+    struct LoopNode;
+    struct BreakNode;
+    struct ContinueNode;
+    struct NullNode;
+    struct DebugNode;
+    struct ModuleNode;
+    struct ImportNode;
+    struct EnumNode;
+    struct ClassNode;
+    struct ClassBodyNode;
+    struct InstanceBodyNode;
+    struct ClassInitializerNode;
+    struct ConstructorDeclNode;
+    struct ConstructorCallNode;
+    struct StaticStatementNode;
+    struct MemberVarNode;
+    struct StaticVarNode;
+    struct IndexNode;
+}
+#endif //ODO_FORWARD_H
