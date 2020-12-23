00dca5bd956ec52f0de58d82b836b171c45e222e
louis1001 <louis1001dev@gmail.com>
2020-12-22 20:20:30 -0600

    Sets the 'is_initialized' flag in the custom loop iterators.
    FoRange and Foreach define variables, but the initialization happens in the c++ side, and at runtime; so I added a little cheat to be able to set it.

--- src/SemAnalyzer/SemanticAnalyzer.cpp
+++ src/SemAnalyzer/SemanticAnalyzer.cpp
@@ -694,6 +694,7 @@ namespace Odo::Semantics {
             }
 
             visit(iterator_decl);
+            currentScope->findSymbol(node->var.value)->is_initialized = true;
 
             visit(node->body);
         } else if (lst_value.type->name == STRING_TP) {
@@ -703,6 +704,7 @@ namespace Odo::Semantics {
                     std::make_shared<NoOpNode>()
             );
             visit(iterator_decl);
+            currentScope->findSymbol(node->var.value)->is_initialized = true;
 
             visit(node->body);
 
@@ -733,7 +735,6 @@ namespace Odo::Semantics {
         }
 
         if (node->second && node->second->kind() != NodeType::NoOp) {
-
             auto second_visited = visit(node->second);
             if (!second_visited.type || !(second_visited.type->name == DOUBLE_TP || second_visited.type->name == INT_TP)) {
                 throw Exceptions::ValueException(
@@ -753,6 +754,7 @@ namespace Odo::Semantics {
                     std::make_shared<NoOpNode>()
             );
             visit(iterator_decl);
+            currentScope->findSymbol(node->var.value)->is_initialized = true;
         }
 
         visit(node->body);
