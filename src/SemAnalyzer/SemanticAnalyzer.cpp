2ebd28d09e182796447d0bee54197323177de123
louis1001 <louis1001dev@gmail.com>
2020-12-24 13:57:54 -0600

    Removes the ondestruction callback for function contexts.

--- src/SemAnalyzer/SemanticAnalyzer.cpp
+++ src/SemAnalyzer/SemanticAnalyzer.cpp
@@ -78,14 +78,7 @@ namespace Odo::Semantics {
 
         functions_context.insert(std::pair(sym, std::move(tps)));
 
-        sym->ondestruction = [this](auto* sym){
-            auto in_global = globalScope.findSymbol(sym->name);
-            if (in_global && in_global == sym){
-                return;
-            }
-            functions_context.erase(sym);
-            semantic_contexts.erase(sym);
-        };
+        sym->ondestruction = [](auto* sym){};
         return func_table;
     }
 
