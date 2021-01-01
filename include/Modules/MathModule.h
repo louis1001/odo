95278da6b796537d54f6dcdfdf8ea417f26ce78c
louis1001 <louis1001dev@gmail.com>
2020-12-31 21:37:57 -0600

    Adds current native math functions to the MathModule.
    Just getting thins ready to transition from the old system.

--- include/Modules/MathModule.h
+++ include/Modules/MathModule.h
@@ -7,6 +7,7 @@
 #define ODO_MATHMODULE_H
 #include "Modules/NativeModule.h"
 #include <numbers>
+#include <cmath>
 
 namespace Odo::Modules {
     class MathModule : public NativeModule{
@@ -17,8 +18,18 @@ namespace Odo::Modules {
             add_literal("e", std::numbers::e);
             add_literal("pi", std::numbers::pi);
 
+            add_function(FACTR_FN, {{int_type, false}}, int_type, [](auto vals){
+                int bound = std::any_cast<int>(vals[0]);
+                int result = 1;
+                for(int i = 1; i <= bound; i++) {
+                    result = result * i;
+                }
+
+                return result;
+            });
+
             add_function(
-                "pow",
+                POW_FN,
                 {{double_type, false}, {double_type, true}},
                 double_type,
                 [&](auto vals){
@@ -27,9 +38,41 @@ namespace Odo::Modules {
                     if (vals.size() == 2) {
                         y = std::any_cast<double>(vals[1]);
                     }
-                    return pow(x, y);
+                    return std::pow(x, y);
                 }
             );
+
+            add_function(SQRT_FN, {{double_type, false}}, double_type, [](auto vals){
+                return std::sqrt(std::any_cast<double>(vals[0]));
+            });
+
+            add_function(SIN_FN, {{double_type, false}}, double_type, [](auto vals){
+                return std::sin(std::any_cast<double>(vals[0]));
+            });
+
+            add_function(COS_FN, {{double_type, false}}, double_type, [](auto vals){
+                return std::cos(std::any_cast<double>(vals[0]));
+            });
+
+            add_function(FLOOR_FN, {{double_type, false}}, int_type, [](auto vals){
+                return (int)std::floor(std::any_cast<double>(vals[0]));
+            });
+
+            add_function(TRUNC_FN, {{double_type, false}}, int_type, [](auto vals){
+                return (int)std::trunc(std::any_cast<double>(vals[0]));
+            });
+
+            add_function(ROUND_FN, {{double_type, false}, {double_type, true}}, double_type, [](auto vals){
+                auto val1 = std::any_cast<double>(vals[0]);
+                double rounding = 0;
+                if (vals.size() == 2) {
+                    rounding = std::any_cast<double>(vals[1]);
+                }
+
+                int decimal = static_cast<int>(std::pow(10, rounding));
+                double result = round(val1*decimal)/decimal;
+                return result;
+            });
 
             add_function("exp", {{double_type, false}}, double_type,
                 [](auto vals){
